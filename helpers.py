ad2157da4625acac0aa395d6436c94498eb78993
evankozliner <evankozliner@gmail.com>
2016-02-02 21:04:08 -0500

    Messed around with naive bayes

--- helpers.py
+++ helpers.py
@@ -8,17 +8,19 @@ from sklearn.metrics import accuracy_score
 
 def cross_validate(training_data, classifier):
     target = extract_target(training_data)
-    #scores = cross_val_score(classifier, training_data, target, cv=5, n_jobs=1)
-    X_train, X_test, y_train, y_test = train_test_split(
-            training_data, target , test_size=.75 , random_state=0)
-    classifier.fit(X_train, y_train)
+    scores = cross_val_score(classifier, training_data, target, cv=10, n_jobs=1)
+    print scores
+    print scores.mean()
+    #X_train, X_test, y_train, y_test = train_test_split(
+    #        training_data, target , test_size=.90 , random_state=0)
+    #classifier.fit(X_train, y_train)
     #scores = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=1)
     #print('CV accuracy scores: %s' % scores)
     #print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
-    y_pred = classifier.predict(X_test)
-    no_samples = len(y_test)
-    print('Misclassified samples: %d of %d' % ((y_test != y_pred).sum() , no_samples))
-    print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+   # y_pred = classifier.predict(X_test)
+   # no_samples = len(y_test)
+   # print('Misclassified samples: %d of %d' % ((y_test != y_pred).sum() , no_samples))
+   # print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
 
 # Extracts a target for cross validation
 def extract_target(data):
@@ -26,13 +28,10 @@ def extract_target(data):
     return results
 
 def clean(data_csv, cleaning_functions):
-    print "clean"
     dataframe = pd.read_csv(data_csv, header=0)
     ids = dataframe['PassengerId'] # Return ids seperately before they're dropped
     for cleaner in cleaning_functions:
         dataframe = cleaner()(dataframe)
-    print "DESCRIBE"
-    print dataframe.describe()
     return ids.values, dataframe.values
 
 def write_predictions(ids, predictions, filename):
--- predict.py
+++ predict.py
@@ -1,7 +1,10 @@
 import helpers
-from sklearn.svm import SVC
 import cleaners as c
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
 
 cleaning_functions = [c.integer_sex_mapping, 
         c.clean_missing_embarks_with_mode,
@@ -13,14 +16,16 @@ cleaning_functions = [c.integer_sex_mapping,
 train_ids, training_data = helpers.clean('data/train.csv', cleaning_functions)
 test_ids, test_data = helpers.clean('data/test.csv', cleaning_functions)
 
+#classifier = GaussianNB()
 #classifier = RandomForestClassifier(n_estimators=100)
-classifier = SVC(kernel='rbf', C=100.0, gamma=0.1, random_state=0)
-helpers.cross_validate(training_data, classifier)
+#classifier = SVC(kernel='rbf', C=100.0, gamma=0.1, random_state=0)
+#classifier = KNeighborsClassifier(n_neighbors=1, weights='uniform', algorithm='auto')
+classifier = BernoulliNB()
+#helpers.cross_validate(training_data, classifier)
 
-# parameters data, ids
-#forest = classifier.fit( training_data[0::, 1::], training_data[0::, 0] )
-#predictions = classifier.predict(test_data).astype(int)
-#helpers.write_predictions(test_ids, predictions, "predictions.csv")
+classifier = classifier.fit( training_data[0::, 1::], training_data[0::, 0] )
+predictions = classifier.predict(test_data).astype(int)
+helpers.write_predictions(test_ids, predictions, "predictions.csv")
 
 def get_function_options():
     return filter(lambda x: not "__" in x, dir(c))
